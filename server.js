bd2afabc7997fc64d1726b064a702153515ff1bc
regisadjihou <rcchadrac@gmail.com>
2021-03-07 20:25:01 -0500

    update database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-cubic-08757',
     user : 'aneagoie',
     password : '',
     database : 'smart-brain'
